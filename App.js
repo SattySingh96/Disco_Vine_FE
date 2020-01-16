aec84c4eb7ed9a685b483c0d616df5b874514e42
sattysingh96 <satnam.bhatti.singh96@gmail.com>
2020-01-16 12:42:35 +0000

    Made changes to styling of ImageThumbnail component, started work on VideoConverter component but decided to make this a backend mechanism, so deleted file

--- App.js
+++ App.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import Navigator from './routes/screenStack';
+import { StyleSheet } from 'react-native'
 
 export default function App() {
-  return <Navigator />;
+  return <Navigator style={styles.Navigator} />;
 }
+
+const styles = StyleSheet.create({
+  Navigator: {
+    flex: 1
+  }
+})
--- screens/slideShow.js
+++ screens/slideShow.js
@@ -1,11 +1,12 @@
-import React, {Component} from 'react';
-import {Text, View} from 'react-native';
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+import VideoConverter from '../components/videoConverter'
 
 export default class home extends Component {
   render() {
     return (
       <View>
-        <Text> SlideShow </Text>
+        <VideoConverter />
       </View>
     );
   }
