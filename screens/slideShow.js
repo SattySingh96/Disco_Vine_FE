6dee628b6891d572ad096ec569f7248a5c162a16
mattg95 <mattgoodman95@gmail.com@>
2020-01-16 10:03:25 +0000

    installed react_video and media_player, created test video in slideshow

--- screens/slideShow.js
+++ screens/slideShow.js
@@ -1,12 +1,30 @@
 import React, {Component} from 'react';
-import {Text, View} from 'react-native';
+import {View, StyleSheet} from 'react-native';
+import Video from 'react-native-video';
 
-export default class home extends Component {
+export default class slideshow extends Component {
   render() {
     return (
       <View>
-        <Text> SlideShow </Text>
+        <Video
+          source={{
+            uri:
+              'https://www.sample-videos.com/video123/mp4/720/big_buck_bunny_720p_1mb.mp4',
+          }}
+          style={styles.backgroundVideo}
+        />
       </View>
     );
   }
 }
+const styles = StyleSheet.create({
+  backgroundVideo: {
+    position: 'absolute',
+    width: 400,
+    height: 400,
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+  },
+});
