3d60fea424ddd5e4e8ab85d88203194d43ccb429
HGartland <harlenski@hotmail.co.uk>
2020-01-17 13:51:29 +0000

    added functioning sound-player to SlideShow screen from react-native-sound with data hardcoded

--- screens/slideShow.js
+++ screens/slideShow.js
@@ -1,11 +1,25 @@
 import React, {Component} from 'react';
 import {Text, View} from 'react-native';
+import SoundPlaya from '../components/SoundPlaya';
+const testSounds = [
+  {
+    url:
+      'https://eu-sounds-bucket.s3.eu-west-2.amazonaws.com/191478__urupin__ping-ping.wav',
+    pose: 'TPose',
+  },
+  {
+    url:
+      'https://eu-sounds-bucket.s3.eu-west-2.amazonaws.com/376523__djfroyd__mystical-sound-sample.wav',
+    pose: 'Dab',
+  },
+];
 
 export default class home extends Component {
   render() {
     return (
       <View>
         <Text> SlideShow </Text>
+        <SoundPlaya soundsToLoad={testSounds} />
       </View>
     );
   }
