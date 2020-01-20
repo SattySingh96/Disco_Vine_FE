23bd5a28fd54c18f4b36fb8f44e609f69f54b222
sattysingh96 <satnam.bhatti.singh96@gmail.com>
2020-01-20 10:36:50 +0000

    Created placeholder screen for the video player if no videos have been created yet

--- screens/gallery.js
+++ screens/gallery.js
@@ -13,6 +13,7 @@ export default class Gallery extends Component {
   state = {
     images: [{ key: 1 }, { key: 2 }, { key: 3 }, { key: 4 }, { key: 5 }, { key: 6 }],
   };
+
   render() {
     return (
       <Fragment>
--- screens/slideShow.js
+++ screens/slideShow.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, Text, StyleSheet, Dimensions } from 'react-native';
 import SoundPlaya from '../components/SoundPlaya';
+import { Button } from 'react-native'
 
 const testSounds = [
   {
@@ -16,18 +17,55 @@ const testSounds = [
 ];
 
 const testImages = [
-  require('/home/satty/Desktop/Disco_Vine_FE/assets/Images/img.png'),
-  require('/home/satty/Desktop/Disco_Vine_FE/assets/Images/002.png'),
-  require('/home/satty/Desktop/Disco_Vine_FE/assets/Images/003.png')
+  // // require('/home/satty/Desktop/Disco_Vine_FE/assets/Images/img.png'),
+  // require('/home/satty/Desktop/Disco_Vine_FE/assets/Images/002.png'),
+  // require('/home/satty/Desktop/Disco_Vine_FE/assets/Images/003.png')
 ];
 
+
 export default class SlideShow extends Component {
+
+
+  renderVideo = () => {
+    if (testImages.length === 0) {
+      return (
+        <View style={styles.background}>
+          <View style={styles.textBox}>
+            <Text style={{ fontSize: 20, textAlignVertical: 'center' }}>No Video?</Text>
+          </View>
+          <View>
+            <Button title='Make one' onPress={() => {
+              this.props.navigation.navigate('Gallery')
+            }} />
+          </View>
+        </View>
+      );
+    }
+    else {
+      return (
+        <SoundPlaya testImages={testImages} soundsToLoad={testSounds} />
+      )
+    }
+  }
+
 
   render() {
     return (
       <View>
-        <SoundPlaya testImages={testImages} soundsToLoad={testSounds} />
+        {this.renderVideo()}
       </View >
     );
   }
 }
+
+const styles = StyleSheet.create({
+  background: {
+    display: 'flex',
+    alignItems: 'center',
+    flexDirection: 'column',
+  },
+  textBox: {
+
+
+  }
+})
