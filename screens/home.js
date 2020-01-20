a3cd82a3c500771bd080f7afcdc682176a0730e6
sattysingh96 <satnam.bhatti.singh96@gmail.com>
2020-01-20 12:43:35 +0000

    Refactored android files back to how they were

--- screens/home.js
+++ screens/home.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
 import { Text, View, StyleSheet, Dimensions } from 'react-native';
 import { TouchableOpacity } from 'react-native';
-import { navigation } from 'react-navigation'
+import LinearGradient from 'react-native-linear-gradient';
 
 export default class home extends Component {
   render() {
     return (
       <View style={styles.HomeContainer}>
-        <TouchableOpacity style={styles.HomeButtons} onPress={() => {
-          this.props.navigation.navigate('Gallery')
-        }}>
-          <Text style={styles.ButtonText}>CREATE</Text>
-        </TouchableOpacity>
-
-
+        <View style={styles.ButtonContainer}>
+          <TouchableOpacity style={styles.HomeButtons} onPress={() => {
+            this.props.navigation.navigate('Gallery')
+          }}>
+            <Text style={styles.ButtonText}>CREATE</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.HomeButtons} onPress={() => {
+            this.props.navigation.navigate('SlideShow')
+          }}>
+            <Text style={styles.ButtonText}>VIEW</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
@@ -30,26 +35,28 @@ const styles = StyleSheet.create({
     height: Dimensions.get('screen').height,
     width: Dimensions.get('screen').width,
   },
+  ButtonContainer: {
+    height: 500,
+    display: 'flex',
+    flexWrap: 'wrap',
+    flexDirection: 'column',
+    justifyContent: 'center'
+  },
   HomeButtons: {
     height: 70,
     width: 160,
     borderStyle: 'solid',
     borderWidth: 1.5,
     borderColor: 'purple',
-    borderRadius: 10,
-    justifyContent: 'center'
+    borderRadius: 12,
+    justifyContent: 'center',
+    margin: 50,
   },
   ButtonText: {
     textAlign: 'center',
     fontFamily: 'sans-serif',
     fontSize: 25,
     color: 'purple'
-
-
-
-
   }
-
-
 
 })
