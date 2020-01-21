c71a3f365303a65f103be82353fc353ebe9ad076
Elizabeth <liz3.33zie@yahoo.co.uk>
2020-01-21 13:20:32 +0000

    added upload image to s3 bucket

--- components/imageThumbnail.js
+++ components/imageThumbnail.js
@@ -1,6 +1,8 @@
 import React, {Fragment, Component} from 'react';
 import {StyleSheet, Image, TouchableOpacity} from 'react-native';
 import ImagePicker from 'react-native-image-picker';
+import {RNS3} from 'react-native-aws3';
+import {accessKey, secretKey} from '../AWSconfig';
 
 export default class ImageThumbnail extends Component {
   state = {
@@ -10,6 +12,7 @@ export default class ImageThumbnail extends Component {
     },
     fileData: '',
     fileUri: '',
+    filePublicURL: '',
   };
   chooseImage = () => {
     const options = {
@@ -18,10 +21,33 @@ export default class ImageThumbnail extends Component {
     };
     ImagePicker.showImagePicker(options, response => {
       const source = {uri: response.uri};
-      this.setState({
-        filePath: response,
-        fileData: response.data,
-        fileUri: response.uri,
+
+      const file = {
+        uri: response.uri,
+        //should make the image name unique- i.e. make username part of image name, maybe take some of response.uri?
+        name: 'userYimage.jpg',
+        //will it always by jpg? Probably, but could take response.uri.subString(-3) unless jpeg...
+        type: 'image/jpg',
+      };
+
+      const options = {
+        keyPrefix: 'images/',
+        bucket: 'eu-image-bucket',
+        region: 'eu-west-2',
+        accessKey: accessKey,
+        secretKey: secretKey,
+        successActionStatus: 201,
+      };
+
+      RNS3.put(file, options).then(s3response => {
+        if (s3response.status !== 201)
+          throw new Error('Failed to upload image to S3');
+        this.setState({
+          filePath: response,
+          fileData: response.data,
+          fileUri: response.uri,
+          filePublicURL: s3response.body.postResponse.location,
+        });
       });
     });
   };
