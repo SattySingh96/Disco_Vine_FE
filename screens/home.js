69e5a92fe8a9b60fa254ea6d285c4a3e61a1dcd5
mattg95 <mattgoodman95@gmail.com@>
2020-01-22 17:11:40 +0000

    added rotating disco ball

--- screens/home.js
+++ screens/home.js
@@ -1,27 +1,35 @@
-import React, {Component} from 'react';
-import {Text, View, StyleSheet, Dimensions} from 'react-native';
-import {TouchableOpacity} from 'react-native';
+import React, { Component } from 'react';
+import { Text, View, StyleSheet, Dimensions, ImageBackground, TouchableOpacity } from 'react-native';
+import { white } from 'react-native-paper/lib/typescript/src/styles/colors';
 
 export default class Home extends Component {
   render() {
     return (
       <View style={styles.HomeContainer}>
-        <View style={styles.ButtonContainer}>
-          <TouchableOpacity
-            style={styles.HomeButtons}
-            onPress={() => {
-              this.props.navigation.navigate('Gallery');
-            }}>
-            <Text style={styles.ButtonText}>CREATE</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.HomeButtons}
-            onPress={() => {
-              this.props.navigation.navigate('SlideShow');
-            }}>
-            <Text style={styles.ButtonText}>VIEW</Text>
-          </TouchableOpacity>
-        </View>
+        <ImageBackground style={{
+          flex: 1,
+          alignSelf: 'stretch',
+          resizeMode: 'stretch',
+          width: undefined,
+          height: undefined,
+        }} source={require('/home/mattg95/Documents/proj/Disco_Vine_FE/assets/Images/giphy.gif')} >
+          <View style={styles.ButtonContainer}>
+            <TouchableOpacity
+              style={styles.HomeButtons}
+              onPress={() => {
+                this.props.navigation.navigate('Gallery');
+              }}>
+              <Text style={styles.ButtonText}>CREATE</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.HomeButtons}
+              onPress={() => {
+                this.props.navigation.navigate('SlideShow');
+              }}>
+              <Text style={styles.ButtonText}>VIEW</Text>
+            </TouchableOpacity>
+          </View>
+        </ImageBackground >
       </View>
     );
   }
@@ -29,36 +37,47 @@ export default class Home extends Component {
 
 const styles = StyleSheet.create({
   HomeContainer: {
-    display: 'flex',
+    flex: 1,
     flexDirection: 'column',
-    alignSelf: 'center',
     justifyContent: 'center',
-    alignContent: 'center',
     alignItems: 'center',
     height: Dimensions.get('screen').height,
     width: Dimensions.get('screen').width,
   },
+  BacgkroundImage: {
+    flex: 1,
+    alignSelf: 'stretch',
+    resizeMode: 'stretch',
+    width: undefined,
+    height: undefined,
+
+  },
+
   ButtonContainer: {
     height: 500,
     display: 'flex',
     flexWrap: 'wrap',
     flexDirection: 'column',
     justifyContent: 'center',
+    alignItems: 'center',
   },
   HomeButtons: {
     height: 70,
     width: 160,
     borderStyle: 'solid',
     borderWidth: 1.5,
-    borderColor: 'purple',
+    borderColor: 'white',
     borderRadius: 12,
+    backgroundColor: 'white',
     justifyContent: 'center',
     margin: 50,
+    alignSelf: 'center',
+
   },
   ButtonText: {
     textAlign: 'center',
     fontFamily: 'sans-serif',
     fontSize: 25,
-    color: 'purple',
+    color: 'white',
   },
 });
